74ac4dddb88af0ae556cc28fd8cca0383c7ed80a
“Danny <danny.seel@epfl.ch>
2022-03-18 15:19:09 +0100

    Primitive (sync) cellulo movement added

--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -14,7 +14,10 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     public override Steering GetSteering()
     {
         Steering steering = new Steering();
-        //implement your code here
+        //implement your code here (to be verified!)
+        steering.linear = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))* agent.maxAccel;
+        steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
+            linear, agent.maxAccel));
         return steering;
     }
 
