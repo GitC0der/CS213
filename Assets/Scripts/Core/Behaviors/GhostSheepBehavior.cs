356e2f63fa4a0bd789b2019254d89f14ee540bc2
“Danny <danny.seel@epfl.ch>
2022-03-19 11:42:47 +0100

    Added 2-player movement (multiplayer)

--- Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
+++ Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
@@ -3,13 +3,34 @@ using UnityEngine;
 
 public class GhostSheepBehavior : AgentBehaviour
 {    
-    public void Start(){
+    public void Start() {
+        this.gameObject.tag = "Sheep";
     }
+
     public override Steering GetSteering()
     {
         
         Steering steering = new Steering();
         //implement your code here.
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        Vector3 playerPositionsSum = new Vector3(0,0,0);
+        foreach(GameObject p in players)
+        {
+            playerPositionsSum += p.transform.position;
+        }
+        Vector3 destination;
+        destination.x = playerPositionsSum.x / players.Length;
+        destination.y = playerPositionsSum.y / players.Length;
+        destination.z = playerPositionsSum.z / players.Length;
+
+        float maxDistanceDelta = 0.5f;
+        Vector3 dst = Vector3.MoveTowards(this.transform.position, destination, maxDistanceDelta);
+
+        steering.linear = dst;
+        steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
+            linear, agent.maxAccel));
+
         return steering;
     }
 
--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -15,7 +15,14 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     {
         Steering steering = new Steering();
         //implement your code here (to be verified!)
-        steering.linear = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))* agent.maxAccel;
+        float horizontalArrows = Input.GetAxis("HorizontalArrows");
+        float verticalArrows = Input.GetAxis("VerticalArrows");
+
+        float horizontalWASD = Input.GetAxis("HorizontalWASD");
+        float verticalWASD = Input.GetAxis("VerticalWASD");
+
+
+        steering.linear = new Vector3(inputKeyboard == 0 ? horizontalArrows : horizontalWASD, 0, inputKeyboard == 0 ? verticalArrows : verticalWASD)* agent.maxAccel;
         steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
             linear, agent.maxAccel));
         return steering;
