3693853cb8cd8b5f23edce0d90d65ddd6b22905b
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2022-03-21 17:23:56 +0100

    Update MoveWithKeyboardBehavior.cs
    Factorize input axis getter

--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 //Input Keys
 public enum InputKeyboard{
-    arrows =0, 
-    wasd = 1
+    Arrows, 
+    WASD
 }
 public class MoveWithKeyboardBehavior : AgentBehaviour
 {
@@ -15,13 +15,10 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     {
         Steering steering = new Steering();
         //implement your code here (to be verified!)
-        float horizontalArrows = Input.GetAxis("HorizontalArrows");
-        float verticalArrows = Input.GetAxis("VerticalArrows");
+        float horizontal = Input.GetAxis("Horizontal" + inputKeyboard.ToString());
+        float vertical = Input.GetAxis("Vertical" + inputKeyboard.ToString());
 
-        float horizontalWASD = Input.GetAxis("HorizontalWASD");
-        float verticalWASD = Input.GetAxis("VerticalWASD");
-
-        steering.linear = new Vector3(inputKeyboard == 0 ? horizontalArrows : horizontalWASD, 0, inputKeyboard == 0 ? verticalArrows : verticalWASD)* agent.maxAccel;
+        steering.linear = new Vector3(horizontal, 0, vertical)* agent.maxAccel;
         steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.
             linear, agent.maxAccel));
         return steering;
