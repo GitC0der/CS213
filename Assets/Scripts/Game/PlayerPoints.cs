0a8439d6e49028dd7cab0150e1998e1b35e9ccea
“Danny <danny.seel@epfl.ch>
2022-03-22 09:32:33 +0100

    PlayerPoints added and RingTrigger updated

---
+++ Assets/Scripts/Game/PlayerPoints.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPoints : MonoBehaviour
+{
+    private int points = 0;
+
+    public void incrementPoints()
+    {
+        ++points;
+    }
+
+    public void decrementPoints()
+    {
+        --points;
+    }
+
+    public int getPoints()
+    {
+        return points;
+    }
+
+    // public static void Main()
+    // {
+    //     RingTrigger rt = new RingTrigger();
+    //     rt.GivePoints += rt_GivePoints; // register with an event
+    //     rt.OnTriggerEnter();
+    // }
+
+    // event handler
+    public static void rt_GivePoints()
+    {
+        incrementPoints();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Game/RingTrigger.cs
+++ Assets/Scripts/Game/RingTrigger.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+public delegate void Notify();
 
 public class RingTrigger : MonoBehaviour
 {
+    public event Notify GivePoints;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +20,30 @@ public class RingTrigger : MonoBehaviour
     {
         
     }
-     void OnTriggerEnter(Collider other){
+    void OnTriggerEnter(Collider other){
         Debug.Log(other.transform.parent.gameObject.name + " triggers.");
+        if (other.CompareTag("Sheep")) {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            GameObject closestPlayer = FindClosestPlayer(players);
+            OnGivePoints();
+            // closestPlayer.incrementPoints;
+        }
+    }
+
+    protected virtual void OnGivePoints()
+    {
+        //if ProcessCompleted is not null then call delegate
+        GivePoints?.Invoke(); 
+    }
+
+
+    private GameObject FindClosestPlayer(GameObject[] players)
+    {
+        GameObject g = players.FirstOrDefault();
+        foreach (GameObject p in players.Skip(1))
+        {
+            g = (p.transform.position - transform.position).magnitude < (g.transform.position - transform.position).magnitude ? p : g;
+        }
+        return g;
     }
 }
