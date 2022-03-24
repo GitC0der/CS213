cccc5ccc42d9a93189d3e6308be9cc3af1c07c88
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2022-03-24 10:07:41 +0100

    Update GameManager.cs
    Made basic structure of GameManager
    Mad GameManager Singleton object

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
-public class GameManager : MonoBehaviour
+public class GameManager
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //used to make the game manager instance pop in the scenes
+    private GameObject gameObject;
 
-    // Update is called once per frame
-    void Update()
+    //Singleton system with an instance of this Game Manager
+    private static GameManager m_Instance;
+    public static GameManager Instance
     {
-                
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = new GameManager();
+                m_Instance.gameObject = new GameObject("_gameManager");
+            }
+            return m_Instance;
+        }
     }
 }
