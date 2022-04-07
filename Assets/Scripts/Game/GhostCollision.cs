009e76a38875dc3576be416737d89199341896cb
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2022-04-07 13:22:09 +0200

    Update GhostCollision.cs
    Add test for ground collision
    Fix collision method name
    Fix collision detection

--- Assets/Scripts/Game/GhostCollision.cs
+++ Assets/Scripts/Game/GhostCollision.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class GhostCollision : MonoBehaviour
 {
-    void onCollisionEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-
+        if (other.gameObject.CompareTag("Ground"))
+            return;
         Debug.Log("Collision occured");
+        Debug.Log("other.collider.transform.parent.gameObject: " + other.collider.transform.parent.gameObject + "   other.collider.gameObject: " + other.collider.gameObject);
 
-        if (other.GetComponentInParent<Transform>().parent.CompareTag("Player")) {
+        if (other.collider.transform.parent.gameObject.CompareTag("Player")) {
 
             Debug.Log("Collision with a player");
 
-            Players.Player player = GameManager.Instance.Players.GetClosestPlayer(other.transform); 
+            Players.Player player = GameManager.Instance.Players.GetClosestPlayer(other.collider.transform); 
             if (!this.GetComponentInParent<GhostSheepBehavior>().GetIsSheep()) {
 
                 Debug.Log("Collision with " + player.name);
