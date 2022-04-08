05385d9ee1606ca02cc3acf086498798ffcc013a
“Danny <danny.seel@epfl.ch>
2022-04-08 11:15:33 +0200

    Text and cellulo position adjusted

--- Assets/Scripts/Game/GhostCollision.cs
+++ Assets/Scripts/Game/GhostCollision.cs
@@ -8,18 +8,10 @@ public class GhostCollision : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
             return;
-        Debug.Log("Collision occured");
         Debug.Log("other.collider.transform.parent.gameObject: " + other.collider.transform.parent.gameObject + "   other.collider.gameObject: " + other.collider.gameObject);
-
         if (other.collider.transform.parent.gameObject.CompareTag("Player")) {
-
-            Debug.Log("Collision with a player");
-
             Players.Player player = GameManager.Instance.Players.GetClosestPlayer(other.collider.transform); 
             if (!this.GetComponentInParent<GhostSheepBehavior>().GetIsSheep()) {
-
-                Debug.Log("Collision with " + player.name);
-
                 player.RemoveScore();
             }
         }
--- Assets/Scripts/Game/RingTrigger.cs
+++ Assets/Scripts/Game/RingTrigger.cs
@@ -25,7 +25,6 @@ public class RingTrigger : MonoBehaviour
     void OnTriggerEnter(Collider other){
         if (other.GetComponentInParent<Transform>().parent.CompareTag("Sheep")) {
             Players.Player cp = GameManager.Instance.Players.GetClosestPlayer(other.transform);
-
             cp.AddScore();
             GetComponent<AudioSource>().Play();
             Debug.Log("Player: " + cp.gameObject.name + " with name: " + cp.name + " has points: " + GameManager.Instance.Players.GetClosestPlayer(other.transform).Score); 
