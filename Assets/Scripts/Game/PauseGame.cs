e185864c9a8f111986e567b4540f4dbac5eb57ee
“Danny <danny.seel@epfl.ch>
2022-04-19 09:20:23 +0200

    Pause menu added

---
+++ Assets/Scripts/Game/PauseGame.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                Cursor.visible = true;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                Cursor.visible = false;
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void unPauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        gamePaused = false;
+        Time.timeScale = 1;   
+    }
+}
