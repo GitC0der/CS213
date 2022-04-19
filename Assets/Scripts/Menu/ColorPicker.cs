1fc88ed9e6ae2dcd5117d43ff9bfc916e86dc78e
“Danny <danny.seel@epfl.ch>
2022-04-19 13:30:30 +0200

    Choices GUI customised

--- Assets/Scripts/Menu/ColorPicker.cs
+++ Assets/Scripts/Menu/ColorPicker.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ColorPicker : AgentBehaviour
 {
+    GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectsWithTag("Player")[0];
     }
 
     // Update is called once per frame
