2d3e2d32cb910e8aa034cc203077847e8fb13892
“Danny <danny.seel@epfl.ch>
2022-04-20 08:20:11 +0200

    Updated ColorPicker

--- Assets/Scripts/Menu/ColorPicker.cs
+++ Assets/Scripts/Menu/ColorPicker.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ColorPicker : AgentBehaviour
 {
+    public int PlayerIdx;
     GameObject Player;
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.FindGameObjectsWithTag("Player")[0];
+        
     }
 
     // Update is called once per frame
@@ -18,18 +19,28 @@ public class ColorPicker : AgentBehaviour
         
     }
 
+    void Initialise()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Players players = GameManager.Instance.Players;
+        Players.Player player1 = players.players[PlayerIdx];
+    }
+
     public void BlueColor()
     {
+        Initialise();
         agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.blue, 0);
     }
 
     public void GreenColor()
     {
+        Initialise();
         agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.green, 0);
     }
 
     public void YellowColor()
     {
+        Initialise();
         agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.yellow, 0);
     }
 }
