6a8f65bf682bad0d6c3f96c1f63fb8a25f2966ff
“Danny <danny.seel@epfl.ch>
2022-04-20 19:06:12 +0200

    Players may now choose their colour

--- Assets/Scripts/Menu/ColorPicker.cs
+++ Assets/Scripts/Menu/ColorPicker.cs
@@ -2,45 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorPicker : AgentBehaviour
+public class ColorPicker : MonoBehaviour
 {
-    public int PlayerIdx;
-    GameObject Player;
-
-    // Start is called before the first frame update
+    CelluloAgent agent;
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void Initialise()
-    {
-        // Player = GameObject.FindGameObjectWithTag("Player");
-        // Players players = GameManager.Instance.Players;
-        // Players.Player player1 = players.players[PlayerIdx];
+    { 
+        agent = GetComponent<CelluloAgent>();
     }
 
     public void BlueColor()
     {
-        // Initialise();
         agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.blue, 0);
     }
 
     public void GreenColor()
     {
-        // Initialise();
         agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.green, 0);
     }
 
     public void YellowColor()
     {
-        // Initialise();
         agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.yellow, 0);
     }
 }
